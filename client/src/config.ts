2d5eb01b14b36042ef6ffbda38688dfbe69cad33
cuongdoan <cuongdoan.brilliant@gmail.com>
2023-05-13 12:06:09 +0700

    add new project

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'pvl3lp7toj';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
+
+export const authConfig = {
+  // Auth0 settings
+  domain: 'dev-0dbfhgxwvlrnirdm.us.auth0.com',
+  clientId: 'dYXRRkyS0cTYEy08xe4QUH1mSmujgOKp',
+  callbackUrl: 'http://localhost:3000/callback'
+};
